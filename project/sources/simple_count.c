eda8f515e66dbacc60b05ff3d80c267f526b7692
Mike <84146116+Mike5535@users.noreply.github.com>
2022-04-01 21:04:46 +0300

    Update simple_count.c

--- project/sources/simple_count.c
+++ project/sources/simple_count.c
@@ -2,25 +2,28 @@
 
 void count_pair(FILE *const input_file, size_t *count) {
 
+  if(!input_file) return;
+  if(!count) return;
+
   for (size_t j = 0; j < NUM_COUNTS; j++) {
     count[j] = 0;
   }
 
-  for (size_t i = 0; i < NUM_COUNTS; i++) {
-    size_t z_o = 0;
-    char c[2];
-    c[z_o] = fgetc(input_file);
-    z_o++;
-    while ((c[z_o] = fgetc(input_file)) != EOF) {
-      if (z_o)
-        z_o--;
-      else
-        z_o++;
+  size_t z_o = 0;
+  char c[2];
+  c[z_o] = fgetc(input_file);
+  z_o++;
+  while ((c[z_o] = fgetc(input_file)) != EOF) {
+    if (z_o)
+      z_o--;
+    else
+      z_o++;
+    for (size_t i = 0; i < NUM_COUNTS; i++)
+    {
       if (c[0] - c[1] == i || c[1] - c[0] == i)
         count[i]++;
     }
-    fseek(input_file, 0, SEEK_SET);
-  }
+    }
 
   return count;
 }
